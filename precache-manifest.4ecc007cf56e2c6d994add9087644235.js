b8da402065806477ceec6fc19b48f0ef2bd71134
VIBA-V <avasyuthechnicals@gmail.com>
2020-05-24 15:40:02 +0530

    Updates

---
+++ precache-manifest.4ecc007cf56e2c6d994add9087644235.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c72ca6bf5f336df506c1c2af9d14a2",
+    "url": "/awareness/index.html"
+  },
+  {
+    "revision": "825a5b2707770ef35ad2",
+    "url": "/awareness/static/css/2.f702fde2.chunk.css"
+  },
+  {
+    "revision": "7a7d4a9232869f30937f",
+    "url": "/awareness/static/css/main.e057f803.chunk.css"
+  },
+  {
+    "revision": "825a5b2707770ef35ad2",
+    "url": "/awareness/static/js/2.b01f1355.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/awareness/static/js/2.b01f1355.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7d4a9232869f30937f",
+    "url": "/awareness/static/js/main.8235e06e.chunk.js"
+  },
+  {
+    "revision": "03a5e16bd0953fa6d406",
+    "url": "/awareness/static/js/runtime-main.c423371a.js"
+  },
+  {
+    "revision": "e6b6afa9a3cb4d40ae51d7c8ac856a23",
+    "url": "/awareness/static/media/1.e6b6afa9.png"
+  },
+  {
+    "revision": "931b72a41fda501e083cecb6f79a263b",
+    "url": "/awareness/static/media/3.931b72a4.png"
+  },
+  {
+    "revision": "d0ca634daa7e3e84018160ad8bd13a0c",
+    "url": "/awareness/static/media/4.d0ca634d.png"
+  },
+  {
+    "revision": "c6fe9acf3a7a2a716399a676953a290f",
+    "url": "/awareness/static/media/5.c6fe9acf.png"
+  },
+  {
+    "revision": "d62ac808e104464b777bbf6695c593c7",
+    "url": "/awareness/static/media/6.d62ac808.png"
+  },
+  {
+    "revision": "454e41fd0280b6a42d8cf9fff8fbb3eb",
+    "url": "/awareness/static/media/7.454e41fd.png"
+  },
+  {
+    "revision": "a7cdb04a611408f398d89cdff705beff",
+    "url": "/awareness/static/media/8.a7cdb04a.png"
+  },
+  {
+    "revision": "ac44dcb72200fc5d8deef772c642dcf5",
+    "url": "/awareness/static/media/9.ac44dcb7.png"
+  },
+  {
+    "revision": "360c127bed16e7137807ec6ecfc60209",
+    "url": "/awareness/static/media/dist.360c127b.png"
+  },
+  {
+    "revision": "1a99f1f2b570089890095b4c9d9c1e53",
+    "url": "/awareness/static/media/doc.1a99f1f2.png"
+  },
+  {
+    "revision": "cd44c4918376797adb0687fd45fe80df",
+    "url": "/awareness/static/media/mm.cd44c491.jpg"
+  },
+  {
+    "revision": "e6631c4c6d38c158e6f75d6628a947a5",
+    "url": "/awareness/static/media/mmm.e6631c4c.jpg"
+  },
+  {
+    "revision": "5a4b547f896990eac3a53003e088be55",
+    "url": "/awareness/static/media/preqs.5a4b547f.jpg"
+  },
+  {
+    "revision": "1f44d174d0b8759cd8df1802961af322",
+    "url": "/awareness/static/media/trophy.1f44d174.jpg"
+  },
+  {
+    "revision": "63b9077f1cdc2851d4ba99300861bdfe",
+    "url": "/awareness/static/media/work.63b9077f.png"
+  }
+]);
